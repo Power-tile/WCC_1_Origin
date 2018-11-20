5de19069b5cf50fd56429c0058616dc033f47013
Species <hejizheng2004@163.com>
2018-11-20 16:33:54 +0800

    generate map basic commit

---
+++ HugeLand/HugeLand/Assets/Resources/Tile.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tile : MonoBehaviour {
+    public int type; //Marking the type of this terrain
+    public bool walkable = true;
+    public bool f
+    
+    void Start() {
+        
+    }
+
+    void Update() {
+        
+    }
+}

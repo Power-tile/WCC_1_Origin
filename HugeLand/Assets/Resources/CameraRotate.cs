a7c269c61699aaec3e8a79b31aa89c4a084a4659
Species <hejizheng2004@163.com>
2018-11-27 09:15:29 +0800

    human merge of camera adding part 1 and init adding commit

---
+++ HugeLand/Assets/Resources/CameraRotate.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraRotate : MonoBehaviour {
+    public void RotateLeft()
+    {
+        int n = 0;
+        if (GameObject.Find("Player1").GetComponent<Camera>().enabled) n = 1;
+        if (GameObject.Find("Player2").GetComponent<Camera>().enabled) n = 2;
+        if (GameObject.Find("Player3").GetComponent<Camera>().enabled) n = 3;
+        if (GameObject.Find("Player4").GetComponent<Camera>().enabled) n = 4;
+        GameObject.Find("Player"+n.ToString()).GetComponent<Camera>().transform.Rotate(Vector3.up, -10, Space.Self);
+    }
+
+    public void RotateRight()
+    {
+        int n = 0;
+        if (GameObject.Find("Player1").GetComponent<Camera>().enabled) n = 1;
+        if (GameObject.Find("Player2").GetComponent<Camera>().enabled) n = 2;
+        if (GameObject.Find("Player3").GetComponent<Camera>().enabled) n = 3;
+        if (GameObject.Find("Player4").GetComponent<Camera>().enabled) n = 4;
+        GameObject.Find("Player" + n.ToString()).GetComponent<Camera>().transform.Rotate(Vector3.up, 10, Space.Self);
+    }
+}

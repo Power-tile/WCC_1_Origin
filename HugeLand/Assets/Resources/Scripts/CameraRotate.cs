2d2bb47f8ad750f4f4d6dc5eec88e5741e7ad085
huangsx0925 <18011018@stu.moonshotacademy.cn>
2018-12-08 01:43:15 +0800

    add final camera rotate with a *****n* angle unit commit

--- HugeLand/Assets/Resources/Scripts/CameraRotate.cs
+++ HugeLand/Assets/Resources/Scripts/CameraRotate.cs
@@ -5,33 +5,17 @@ using System;
 
 public class CameraRotate : MonoBehaviour {
 
-    public int differ; //recording the angle difference between current rotate and initial rotate
+    public float differ; //recording the angle difference between current rotate and initial rotate
 
     public void RotateLeft()
     {
-        int n = 0;
-        if (GameObject.Find("Player1").transform.Find("Camera").gameObject.GetComponent<Camera>().enabled) { n = 1; }
-        if (GameObject.Find("Player2").transform.Find("Camera").gameObject.GetComponent<Camera>().enabled) { n = 2; }
-        if (GameObject.Find("Player3").transform.Find("Camera").gameObject.GetComponent<Camera>().enabled) { n = 3; }
-        if (GameObject.Find("Player4").transform.Find("Camera").gameObject.GetComponent<Camera>().enabled) { n = 4; }
-        GameObject CurrentPlayer = GameObject.Find("Player" + n.ToString());
-        GameObject CurrentCamera = GameObject.Find("Player" + n.ToString()).transform.Find("Camera").gameObject;
-        CurrentCamera.transform.RotateAround(CurrentPlayer.transform.position,Vector3.up,5);
-        differ += 5;
+        differ += 10f;
         //CurrentPlayer.transform.rotation = Quaternion.Euler(0.0f, CurrentPlayer.transform.rotation.y - 10.0f, 0.0f);
     }
 
     public void RotateRight()
     {
-        int n = 0;
-        if (GameObject.Find("Player1").transform.Find("Camera").gameObject.GetComponent<Camera>().enabled) { n = 1; }
-        if (GameObject.Find("Player2").transform.Find("Camera").gameObject.GetComponent<Camera>().enabled) { n = 2; }
-        if (GameObject.Find("Player3").transform.Find("Camera").gameObject.GetComponent<Camera>().enabled) { n = 3; }
-        if (GameObject.Find("Player4").transform.Find("Camera").gameObject.GetComponent<Camera>().enabled) { n = 4; }
-        GameObject CurrentPlayer = GameObject.Find("Player" + n.ToString());
-        GameObject CurrentCamera = GameObject.Find("Player" + n.ToString()).transform.Find("Camera").gameObject;
-        CurrentCamera.transform.RotateAround(CurrentPlayer.transform.position, Vector3.up, -5);
-        differ -= 5;
+        differ -= 10f;
         //CurrentPlayer.transform.rotation = Quaternion.Euler(0.0f, CurrentPlayer.transform.rotation.y + 10.0f, 0.0f);
     }
 
@@ -40,43 +24,34 @@ public class CameraRotate : MonoBehaviour {
 
     void Start()
     {
-        //check player facing direction changing
-        PrevDir[1] = GameObject.Find("Player1").transform.forward;
-        PrevDir[2] = GameObject.Find("Player2").transform.forward;
-        PrevDir[3] = GameObject.Find("Player3").transform.forward;
-        PrevDir[4] = GameObject.Find("Player4").transform.forward;
+        
     }
 
     void Update()
     {
-        Dir[1] = GameObject.Find("Player1").transform.forward;
-        Dir[2] = GameObject.Find("Player2").transform.forward;
-        Dir[3] = GameObject.Find("Player3").transform.forward;
-        Dir[4] = GameObject.Find("Player4").transform.forward;
-        for (int i=1;i<=4;i++)
-        {
-            GameObject player = GameObject.Find("Player" + i.ToString());
-            GameObject camera = GameObject.Find("Player" + i.ToString()).transform.Find("Camera").gameObject;
-            float Dirx = (float)System.Math.Round(Dir[i].x);
-            float Dirz = (float)System.Math.Round(Dir[i].z);
-            float PrevDirx = (float)System.Math.Round(PrevDir[i].x);
-            float PrevDirz = (float)System.Math.Round(PrevDir[i].z);
-            if ((-PrevDirz == Dirx) && (PrevDirx == Dirz))
-            {
-                camera.transform.RotateAround(player.transform.position, Vector3.up, 45);
-            }
-            else if ((-PrevDirx == Dirx) && (-PrevDirz == Dirz))
-            {
-                camera.transform.RotateAround(player.transform.position, Vector3.up, 90);
-            }
-            else if ((PrevDirz == Dirx) && (-PrevDirx == Dirz))
-            {
-                camera.transform.RotateAround(player.transform.position, Vector3.up, -  45);
-            }
-        }
-        PrevDir[1] = Dir[1];
-        PrevDir[2] = Dir[2];
-        PrevDir[3] = Dir[3];
-        PrevDir[4] = Dir[4];
+        GameObject Player = GameObject.Find("Player1");
+        GameObject Camera = Player.transform.Find("Camera").gameObject;
+        float CurrentAngle = differ;
+        
+        Vector3 rotation = transform.localEulerAngles;
+        rotation.y = CurrentAngle - System.Math.Sign(Player.transform.rotation.y)
+                                   *Player.transform.rotation.y
+                                   *Player.transform.rotation.y
+                                   *180;
+        //It seems that the fucking unity thinks that the constant PI should be square root 2. 
+        transform.localEulerAngles = rotation;
+        
+        Vector3 PlayerPos = Player.transform.position;
+        Vector3 CameraPos = Player.transform.Find("Camera").gameObject.transform.position;
+        Vector3 vector1 = Vector3.back*6+Vector3.up*4;
+        float x = vector1.x;
+        float z = vector1.z;
+        float sin_a = (float)System.Math.Sin((double)CurrentAngle / 180 * 3.1415926);
+        float cos_a = (float)System.Math.Cos((double)CurrentAngle / 180 * 3.1415926);
+        Vector3 vector2 = vector1;
+        vector2.x = cos_a * x + sin_a * z;
+        vector2.z = cos_a * z - sin_a * x;
+        CameraPos = PlayerPos + vector2;
+        Player.transform.Find("Camera").gameObject.transform.position = CameraPos;
     }
 }
